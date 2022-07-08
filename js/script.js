77835c3411219f16c7915ab5478637efe48afd9f
Trigo YODILA <trigoyodila1996@gmail.com>
2022-07-08 16:09:56 +0200

    modification style du bloc login-form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let navbar = document.querySelector('.header .navbar');
 let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
 
 document.querySelector('#menu-btn').addEventListener('click',()=>{
     navbar.classList.toggle('active');
@@ -9,7 +10,12 @@ document.querySelector("#search-btn").addEventListener("click", () => {
   searchForm.classList.toggle("active");
 }); 
 
+document.querySelector("#login-btn").addEventListener("click", () => {
+  loginForm.classList.toggle("active");
+}); 
+
 window.addEventListener('scroll',()=>{
     navbar.classList.remove('active');
     searchForm.classList.remove("active");
+    loginForm.classList.remove("active");
 })
