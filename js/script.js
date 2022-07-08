a9f6f82c121de052e86ad9ad0808a720ac5073c9
Trigo YODILA <trigoyodila1996@gmail.com>
2022-07-08 14:38:59 +0200

    changement style bloc recherche

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
 
 document.querySelector('#menu-btn').addEventListener('click',()=>{
     navbar.classList.toggle('active');
 })
 
+document.querySelector("#search-btn").addEventListener("click", () => {
+  searchForm.classList.toggle("active");
+}); 
+
 window.addEventListener('scroll',()=>{
     navbar.classList.remove('active');
+    searchForm.classList.remove("active");
 })
