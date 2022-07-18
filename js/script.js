98ad625c88c3c8078486e2db00d7cd4c7a970a53
Trigo YODILA <trigoyodila1996@gmail.com>
2022-07-18 15:52:34 +0200

    style du contact info

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-let navbar = document.querySelector('.header .navbar');
-let searchForm = document.querySelector('.header .search-form');
-let loginForm = document.querySelector('.header .login-form');
+let navbar = document.querySelector(".header .navbar");
+let searchForm = document.querySelector(".header .search-form");
+let loginForm = document.querySelector(".header .login-form");
+let contactinfo = document.querySelector(".contact-info");
 
-document.querySelector('#menu-btn').addEventListener('click',()=>{
-    navbar.classList.toggle('active');
-})
+document.querySelector("#menu-btn").addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
 
 document.querySelector("#search-btn").addEventListener("click", () => {
   searchForm.classList.toggle("active");
-}); 
+});
 
 document.querySelector("#login-btn").addEventListener("click", () => {
   loginForm.classList.toggle("active");
-}); 
+});
 
-window.addEventListener('scroll',()=>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove("active");
-    loginForm.classList.remove("active");
-})
+document.querySelector("#info-btn").addEventListener("click", () => {
+  contactinfo.classList.add("active");
+});
+document.querySelector("#close-contact-info").addEventListener("click", () => {
+  contactinfo.classList.remove("active");
+});
+
+window.addEventListener("scroll", () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  contactinfo.classList.remove("active");
+});
